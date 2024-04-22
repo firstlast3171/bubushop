4b1adfd433f2549896df2e6a358cd69346b0bd18
BhoneThit <firstlast3171@gmail.com>
2024-04-22 14:24:42 +0630

    ToShowTest

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           <Route path='/' element={<Layout />}>
               <Route path='/' element={<Home />} />
               <Route path='/about' element={<About />} />
-              <Route path='*' element={<Nopage />} />
+              <Route path='*' element={<Home />} />
           </Route>
       </Routes>
     </BrowserRouter>
